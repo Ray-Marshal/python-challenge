f94735e8538bc0e90a145f3bbb134f271e7fe1f4
Marshal Rittenger <mrrittenger@gmail.com>
2024-01-01 21:14:48 -0800

    FINAL CODE for Mod 3 Challenge + txt results files

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,70 +2,65 @@
 import os
 import csv
 
+#Establishing variables
+#filepath = '../Resources/budget_data.csv' - I could never get this method to work, so I opted for what you see below
 filepath = ('C:\\Users\\mrrit\\OneDrive\\Documents\\UofO Data Class\\Wk3_Python\\python-challenge\\PyBank\\Resources\\budget_data.csv')
 total_months = 0
-net_profit_loss = 0
+net_profit_or_loss = 0
 great_increase = 0
 great_decrease = 0
 this_month = 0
-tot_cume_change = 0
 row_num = 0
 average_change = 0
+previous_month = None
+monthly_change = 0
 
-
+#opening the file and skipping the header row
 with open(filepath, "r", encoding='utf') as budget_file:
     
     budget_reader = csv.reader(budget_file, delimiter=",")
-    last_row = sum(1 for _ in budget_reader)
-    #last_row = index.budget_reader[-1]
+    #last_row = sum(1 for _ in budget_reader)
     budget_file.seek(0)
     next(budget_reader, None)
 
     for row in (budget_reader):
-        last_month = this_month
-        this_month = int(row[1])
-        total_change = this_month - last_month
+
+        #Calculating the month to month changes excluding 0 --> first month change
+        if row_num == 0:
+            this_month = int(row[1])
+        else:    
+            previous_month = this_month
+            this_month = int(row[1])
+            monthly_change = this_month - previous_month
+        
+        #finding the date for code below, and adding all of the data we needed from this row to the cumulative variables 
         date = row[0]
         total_months += 1
-        net_profit_loss += this_month
-        average_change += total_change
-
-        if row_num == 0:
-            first_month = this_month
-
+        net_profit_or_loss += this_month
+        average_change += monthly_change
         row_num += 1
 
-        if total_change > great_increase:
-            great_increase = total_change
+        #Adding the motnthly changes to greatest increase or decrease as necessary
+        if monthly_change > great_increase:
+            great_increase = monthly_change
             great_inc_date = date
 
-        if total_change < great_decrease:
-            great_decrease = total_change
+        if monthly_change < great_decrease:
+            great_decrease = monthly_change
             great_dec_date = date
 
-        if row_num == last_row - 1:
-            tot_cume_change = last_month - first_month
+    average_change = average_change / (total_months - 1)
 
-    average_change = average_change / total_months
+    #printing results to terminal
+    print("Financial Analysis \n-------------------------")
+    print(f"Total Months: {total_months} \nTotal: ${net_profit_or_loss} \nAvereage Change: ${round(average_change, 2)}")
+    print(f"Greatest Increase in Profits: {great_inc_date} (${great_increase}) \nGreatest Decrease in Profits: {great_dec_date} (${great_decrease})")
 
-    print("Financial Analysis")
-    print("-------------------------")
-    print(f"Total Months: {total_months}")
-    print(f"Total: ${net_profit_loss}")
-    print(f"Avereage Change: ${average_change}")
-    print(f"Greatest Increase in Profits: {great_inc_date} (${great_increase})")
-    print(f"Greatest Decrease in Profits: {great_dec_date} (${great_decrease})")
-
+    #writing results to new text file
     pybank_results_file = open("pybank_results_file.txt", "w")
-    print("Financial Analysis", file=pybank_results_file)
-    print("-------------------------", file=pybank_results_file)
-    print(f"Total Months: {total_months}", file=pybank_results_file)
-    print(f"Total: ${net_profit_loss}", file=pybank_results_file)
-    print(f"Avereage Change: ${average_change}", file=pybank_results_file)
-    print(f"Greatest Increase in Profits: {great_inc_date} (${great_increase})", file=pybank_results_file)
-    print(f"Greatest Decrease in Profits: {great_dec_date} (${great_decrease})", file=pybank_results_file)
+    print("Financial Analysis \n-------------------------", file=pybank_results_file)
+    print(f"Total Months: {total_months} \nTotal: ${net_profit_or_loss} \nAvereage Change: ${round(average_change, 2)}", file=pybank_results_file)
+    print(f"Greatest Increase in Profits: {great_inc_date} (${great_increase}) \nGreatest Decrease in Profits: {great_dec_date} (${great_decrease})", file=pybank_results_file)
 
     pybank_results_file.close()
-    budget_file.close()
-    
-#print(last_row)
+budget_file.close()
--- Pypoll/main.py
+++ Pypoll/main.py
@@ -2,6 +2,8 @@
 import os
 import csv
 
+#establishing variables
+#filepath = '../Resources/election_data.csv' - I could never get this method to work, so I opted for what you see below
 filepath = ('C:\\Users\\mrrit\\OneDrive\\Documents\\UofO Data Class\\Wk3_Python\\python-challenge\\PyPoll\\Resources\\election_data.csv')
 poll_results_file = None
 total_votes = None
@@ -10,6 +12,8 @@ candidate_num = None
 percent = None
 winner = None
 final_results = None
+
+#establishing empty lists
 candidates = []
 votes_per_candidate = []
 vote_percentages = []
@@ -22,6 +26,7 @@ with open(filepath, "r", encoding='utf') as poll_file:
     for row in poll_reader:
         candidate_name = row[2]
 
+        #adding votes if the candidate is already in the list, else appending new elements to the lists
         if candidate_name in candidates:
             candidate_num = candidates.index(candidate_name)
             votes_per_candidate[candidate_num] +=1 
@@ -29,23 +34,29 @@ with open(filepath, "r", encoding='utf') as poll_file:
             candidates.append(candidate_name)
             votes_per_candidate.append(1)
 
+    #finding the total votes by adding all of the numbers in the 'votes per candidate' list
     total_votes = sum(votes_per_candidate[0:(len(candidates))])
 
+    #finding the vote percentages by dividing votes per candidate by total votes, element by element
     for i in votes_per_candidate:
         percent = (i/total_votes)*100
         vote_percentages.append(percent)
 
+    #the winner had the 'max' number of votes
     winner = candidates[votes_per_candidate.index(max(votes_per_candidate))] 
-    final_results = zip(candidates, vote_percentages, votes_per_candidate)
-    poll_results_file = open("results_file.txt", "w")
 
+    #zipping it all together for easier printing    
+    final_results = zip(candidates, vote_percentages, votes_per_candidate)
+    
+    #printing results to terminal
     print(f"Election Results \n-------------------------------- \nTotal Votes: {total_votes}")
     for i in final_results:
         print(f"{i[0]}: {round(i[1], 3)}% ({i[2]})")
     print(f"-------------------------------- \nWinner: {winner} \n--------------------------------")
 
+    #writing results to new text file
     final_results = zip(candidates, vote_percentages, votes_per_candidate)
-    
+    poll_results_file = open("pypoll_results_file.txt", "w")
     print(f"Election Results \n-------------------------------- \nTotal Votes: {total_votes}", file=poll_results_file)
     for i in final_results:
         print(f"{i[0]}: {round(i[1], 3)}% ({i[2]})", file=poll_results_file)
